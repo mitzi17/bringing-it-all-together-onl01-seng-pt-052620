22b00d0b52ad7311a22853cc7e6604dd11bd6e1b
Mitzi Estrada <m_estrada17@live.com>
2020-08-02 06:55:05 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -37,9 +37,9 @@ class Dog
     WHERE name = ?
     SQL
     
-    DB[:conn].execute(sql)
+    DB[:conn].execute(sql, name).map do |row|
     self.new_from_db(row)
-    
+    end.first
   end
     
   end
