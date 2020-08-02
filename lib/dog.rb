5f778bca2daa12dd322f81607245ed32cc58c989
Mitzi Estrada <m_estrada17@live.com>
2020-08-02 06:50:02 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -23,10 +23,11 @@ class Dog
     DB[:conn].execute(sql)
   end
   
-  def new_from_db(row)
+  def self.new_from_db(row)
     id = row[0]
     name = row[1]
     breed =row [2]
+    
   end
     
   end
