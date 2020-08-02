0b38e5f552a8c22f3bde00f3446e38058d9dbd7b
Mitzi Estrada <m_estrada17@live.com>
2020-08-02 07:03:32 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -55,7 +55,7 @@ class Dog
       SQL
       
       DB[:conn].execute(sql, name, breed)
-      @id = DB[:conn].execute(SELECT last_insert_rowid() FROM dogs)
+      @id = DB[:conn].execute("SELECT last_insert_rowid() FROM dogs")[0][0]
   end
     
   
